23452dd0a57b39bb4e2cd9d4a1ff9a73d095e5ae
hoantrung112 <55022560+hoantrung112@users.noreply.github.com>
2019-10-27 21:30:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-async function getUsers () {
-    const rawResponse= await fetch("https://gateway.marvel.com:443/v1/public/characters?&ts=12345&apikey=1cb8f124cfacf199eb92aa85d67dd9b3&hash=7867bd775a8bd67600140ee13563af61");
-    const response = await rawResponse.json();
-    console.log(response);
-    for(let i=0;i<20;i++){
-        document.getElementById("users-list").innerHTML +=`  
-        <div>
-          <p>${response.data.results[i].id} ${response.data.results[i].name} </p> 
-          <p>${response.data.results[i].modified} </p>
-        </div>
-        `;
-        response.data.results[i];
-    }      
-}
- getUsers();
+document.getElementById('span3').addEventListener('mouseover', function () {
+    document.getElementById('dropdown').style.display = 'block';
+});
+document.getElementById('dropdown').addEventListener('mouseleave', function () {
+    document.getElementById('dropdown').style.display = 'none';
+})
