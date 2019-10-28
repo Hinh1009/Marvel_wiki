77dcdfcce19ac45e01bda752de9409b75e4515a2
Hinh1009 <50358382+Hinh1009@users.noreply.github.com>
2019-10-28 14:09:45 +0700

    Update a.js

--- pagenamestart/a.js
+++ pagenamestart/a.js
@@ -1,9 +1,9 @@
 async function getUsers() {
     var c = "a";
-    const rawResponse = await fetch("https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + c + "&limit=50&ts=12345&apikey=1cb8f124cfacf199eb92aa85d67dd9b3&hash=7867bd775a8bd67600140ee13563af61");
+    const rawResponse = await fetch("https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + c + "&limit=60&ts=12345&apikey=1cb8f124cfacf199eb92aa85d67dd9b3&hash=7867bd775a8bd67600140ee13563af61");
     const response = await rawResponse.json();
     console.log(response);
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 60; i++) {
         document.getElementById("users-list").innerHTML += `  
         <div>
         <img src="${response.data.results[i].thumbnail.path}.jpg"/>
