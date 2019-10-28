52508761b01953c78b94c26182a725922d329b56
nguyenvandong26396 <54853488+nguyenvandong26396@users.noreply.github.com>
2019-10-28 08:13:02 +0700

    add_ironman

---
+++ ironman.js
@@ -0,0 +1,39 @@
+async function getUsers () {
+    const rawResponse= await fetch("https://gateway.marvel.com:443/v1/public/characters/1009368?ts=12345&apikey=1cb8f124cfacf199eb92aa85d67dd9b3&hash=7867bd775a8bd67600140ee13563af61");
+    const response = await rawResponse.json();
+    console.log(response);
+    document.getElementById("users-list").innerHTML +=`
+        <div class="item">
+            <div class="text-container">
+               <h1>${response.data.results[0].name}</h1>
+            </div>
+            <div>
+               <p>Description:${response.data.results[0].description}</p>
+            </div>
+           <div class="main">
+               <img src="${response.data.results[0].thumbnail.path}.jpg">
+           <div>
+           <div class="text-container">
+               <p>Id: ${response.data.results[0].id}</p>
+            </div>
+            <div class="text-container">
+               <p>Modified:${response.data.results[0].modified}</p>
+            </div>
+           <h3>Các bộ phim iron man đóng:</h3>
+        </div>
+        `;
+    for(let i=0;i<20;i++){
+        document.getElementById("users-list").innerHTML +=`
+        <div class="text">
+          <div> 
+            <p>${response.data.results[0].comics.items[i].name} </p>
+          </div>
+          <div> 
+             <p>${response.data.results[0].events.items[i].name} </p>
+          </div>
+        </div>
+        `;
+        response.data.results[i];
+    }   
+} 
+getUsers();
